b08ac1130dce8f3e594122c003dcadc4903d3043
Tresna Eka Widiana <ideal.nagi@gmail.com>
2023-10-31 21:32:31 +0700

    video 9

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,63 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Tresna Eka Widiana',
+            'email' => '193040071@mail.unpas.ac.id',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Dian Nuryana',
+            'email' => '193040045@mail.unpas.ac.id',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem yang sedikit',
+            'body' => 'Lorem yang buanyak',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem yang sedikit',
+            'body' => 'Lorem yang buanyak',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem yang sedikit',
+            'body' => 'Lorem yang buanyak',
+            'category_id' => 1,
+            'user_id' => 2
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem yang sedikit',
+            'body' => 'Lorem yang buanyak',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,8 +4,8 @@
 @section('container')
 <article>
     <h2>{{ $post->title }}</h2>
-    <p>By. Tresna Eka Widiana in <a href="/categories/{{ $post->category->name }}">{{ $post->category->name }}</a></p>
+    <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->slug }}</a></p>
    {!! $post->body !!}
 </article>
-<a href="/posts">Back To Post</a>
+<a href="/posts" class="d-block mt-3">Back To Post</a>
 @endsection
