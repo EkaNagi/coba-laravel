261913d43124e0a6e19daf215a47e2c431527fc1
Tresna Eka Widiana <ideal.nagi@gmail.com>
2023-10-31 20:55:27 +0700

    video 8

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,7 @@
 @section('container')
 <article>
     <h2>{{ $post->title }}</h2>
+    <p>By. Tresna Eka Widiana in <a href="/categories/{{ $post->category->name }}">{{ $post->category->name }}</a></p>
    {!! $post->body !!}
 </article>
 <a href="/posts">Back To Post</a>
