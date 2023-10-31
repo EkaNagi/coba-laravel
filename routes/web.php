271227fc71bd3a9e5094e9110eb143d5ec8ce6f4
Tresna Eka Widiana <ideal.nagi@gmail.com>
2023-10-31 12:45:44 +0700

    video 3

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Tresna Eka Widiana",
+        "email" => "193040071@mail.unpas.ac.id",
+        "image" => "image-about.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
