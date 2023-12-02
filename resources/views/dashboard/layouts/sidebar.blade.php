e606dc27af77dc9dc509f8676a2f33740c3c3a32
Tresna Eka Widiana <ideal.nagi@gmail.com>
2023-12-03 00:23:34 +0700

    video 23

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -19,6 +19,19 @@
             </a>
           </li>
         </ul>
+        @can('admin')
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+          <span>Administrator</span>
+        </h6>
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link d-flex align-items-center gap-2 {{ Request::is('dashboard/categories*') ? 'active' : '' }}" href="/dashboard/categories">
+              <svg class="bi"><use xlink:href="#file-earmark"/></svg>
+              Post Categories
+            </a>
+          </li>
+        </ul>
+        @endcan
 
         <hr class="my-3">
 
